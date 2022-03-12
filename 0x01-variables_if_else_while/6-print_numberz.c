05e161550311cd25a5a09ea1bf0f42096cb24b9f
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-03-12 10:04:46 -0800

    Create 6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+*main - print lowercase alpha a-z but remove 'q' and 'e'
+*Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+  int  num = 0;
+  
+  while(num < 9)
+  {
+    
+  }
+    
+	return (0);
+}
