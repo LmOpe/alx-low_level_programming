be6404a6f39e61fff18c59f1627ee4cd12dfac2c
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-12 11:42:01 -0800

    Variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- *main - print all combinations of three different digits
- *Return: Always 0 (Success)
+ * main - print all combinations of three different digits
+ * Return: Always 0 (Success)
  */
 
 int main(void)
