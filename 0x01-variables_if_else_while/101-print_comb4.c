9d3e9b6aa5c9eb8496a30779c09ddf15ab0cbbba
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-12 12:02:36 -0800

    Variables

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -11,7 +11,7 @@ int main(void)
 	int ten;
 	int hundred;
 
-	for (hundred = '0'; hundreed <= '9'; hundred++)
+	for (hundred = '0'; hundred <= '9'; hundred++)
 	{
 		for (ten = (hundred + 1); ten <= '9'; ten++)
 		{
