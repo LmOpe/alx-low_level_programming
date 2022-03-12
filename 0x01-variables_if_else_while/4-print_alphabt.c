3223508f11d59ec12a32289fb1427917c453217e
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-12 09:45:57 -0800

    Variables

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
-*main - print lowercase alpha a-z but remove 'q' and 'e'
-*Return: Always 0 (Success)
+ *main - print lowercase alpha a-z but remove 'q' and 'e'
+ *Return: Always 0 (Success)
 */
 
 int main(void)
