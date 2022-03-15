705706f80162b4889e2a95548d721a5749100c86
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-15 13:30:13 -0700

    Nested loops

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include "main.h"
-
 /**
  * main - Entry point
  * Return: Always 0 (success)
+ * print_alphabet - prints alphabets in lower case
  */
 
 void print_alphabet(void)
