996a3f7bfe0fee93f43fafa38d5f72bfb8d78c01
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-15 14:40:23 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * _isalpha - checks for lower or uppercase alphabets
+ * @c: character to check
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int _isalpha(int c)
+{
+	if (c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
