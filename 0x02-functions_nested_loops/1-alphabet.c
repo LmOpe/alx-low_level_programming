47d6f27de81cefac3ae2b5b2b4cfefc34f76fae5
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-15 12:52:14 -0700

    Nested loops

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char alpha = 'a';
+	while (alpha <= 'z')
+	{
+		putchar("%c\n", alpha);
+		alpha++;
+		return (0);
+	}
+}
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
