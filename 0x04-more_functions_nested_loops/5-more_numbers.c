0cf1249b67e8b0c70411b2f5816095c82040babc
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-17 16:43:29 -0700

    Loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,8 +14,12 @@ void more_numbers(void)
 	{
 		for (num = '0'; num <= '14'; num++)
 		{
-			_putchar(num);
+			if (num >= '10')
+			{
+				_putchar((num / '10') + '0');
+			}
+			_putchar((num % '10') + '0');
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
