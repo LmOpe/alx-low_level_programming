78e3ad2af3312acf1938dbe2d4ade2f776c48135
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-17 08:08:25 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * positive_or_negative: function to test
+ * main - Entry point
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	positive_or_negative(0);
+
+	return (0);
+}
