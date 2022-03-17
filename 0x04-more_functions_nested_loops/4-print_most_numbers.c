d3754911b8b1e33b7d1f26473f815362126221d0
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-17 15:57:20 -0700

    Loops

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints all integers except 2 and 4
+ * main - Entry point
+ * Return: 0
+ */
+
+void print_most_numbers(void)
+{
+	char num = '0';
+
+	while (num <= '9')
+	{
+		if (num != 2 && != 4)
+		{
+			_putchar(num);
+		}
+		num++;
+	}
+	_putchar('\n');
+}
