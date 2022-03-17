aad214481fde7f06f17fc5cc37d9cb04907f30c5
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-03-16 20:25:00 -0700

    Update 102-fibonacci.c

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -14,7 +14,14 @@ int main(void)
 
 	for (counter = 1; counter <= (countto / 2); counter++)
 	{
-		printf("%li, %li, ", a, b);
+		if (counter < (countto / 2))
+		{
+			printf("%li, %li, ", a, b);
+		}
+		else
+		{
+			printf("%li, %li ", a, b);
+		}
 		a += b;
 		b += a;
 	}
