7b0e7dee0dec9d362ae6c0c2c734b059bcfe9296
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-21 19:52:31 -0700

    Pointers

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
- * reset_to_98 - function that updates the value of the parameter points to to 98
+ * reset_to_98 - function that updates the value the param points to to 98
  * @n: the parameter to change its value
  */
 
