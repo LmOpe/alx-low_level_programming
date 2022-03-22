5edd1e6aeb2cf16c2402ef491eb4b7ae828d2f0e
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-21 20:28:43 -0700

    Pointers

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
-/** _strlen - function that returns the length of a string
+/**
+ * _strlen - function that returns the length of a string
  * @s: the parameter its length is returned
  * Return: the legnth of the string
  */
@@ -8,9 +9,10 @@
 int _strlen(char *s)
 {
 	int i;
+
 	i = 0;
 
-	while (s[ i ] != '\0')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
