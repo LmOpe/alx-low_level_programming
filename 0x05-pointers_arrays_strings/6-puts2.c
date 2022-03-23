aa2038839f53f9e3c7c4ef4a0007cb5fbb4e371a
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-22 20:59:08 -0700

    Pointer

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * puts2 - function that prints evey other charater of a string
+ * @str: string to be printed
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+}
