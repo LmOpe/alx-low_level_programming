827f664790b83dd6958c92a76f1c6fa4779afc24
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-03-22 19:54:37 -0700

    Create 5-rev_string.c

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/**
+ * _strlen - function that returns the length of a string
+ * @s: the string that its length is printed
+ * Return: the legnth of the string
+ */
+
+int _strlen(char *s)
+{
+	int i;
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
+
+/**
+ * rev_string - function that prints a string in reverse
+ * @s: the string that is reversed
+ */
+
+void rev_string(char *s)
+{
+	int len;
+
+	len = _strlen(s);
+
+	len -= 1;
+
+	while (len >= 0)
+	{
+		s += putchar(s[len]);
+		len--;
+	}
+}
