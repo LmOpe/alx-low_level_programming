d8ed6824ff6348189b5da0a2aefa8b7fbb66f964
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-22 18:53:12 -0700

    Pointers

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,1 +1,21 @@
 #include <stdio.h>
+
+/**
+ * print_rev - function that prints a string in reverse
+ * @s: the string that is printed
+ */
+
+void print_rev(char *s)
+{
+	int i;
+
+	i = strlen(s);
+
+	while (i >= 0)
+	{
+		i -= 1;
+
+		printf("%s\n", s[i]);
+		i--;
+	}
+}
