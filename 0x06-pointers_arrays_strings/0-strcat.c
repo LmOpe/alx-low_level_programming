da468510f3ff7b562e7fd38c18aa6f75f3ccd040
Lawal Muhammed Opeyemi <lawalmuhammedope@gmail.com>
2022-03-24 14:52:46 -0700

    Pointer

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,6 +4,7 @@
  * _strcat- function that concatenates two strings
  * @dest: the first string
  * @src: the second string
+ * Return: concatenated string
  */
 
 char *_strncat(char *dest, char *src, int n)
@@ -11,10 +12,10 @@ char *_strncat(char *dest, char *src, int n)
 	int i = 0;
 	int j = 0;
 
-	while (dest[i] != '\0') /*i is index of null terminator*/
+	while (dest[i] != '\0')
 		i++;
 
-	while (src[j] != src[n]) /*append replacing null terminator*/
+	while (src[j] != src[n])
 	{
 		dest[i] = src[j];
 		i++;
