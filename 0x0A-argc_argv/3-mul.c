2e9626d56b62c83450ae8da45b2f6b7c60011ad9
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-01 08:10:30 -0700

    Update 3-mul.c

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,12 +11,13 @@
 int main(int argc, char *argv[])
 {
 	int res;
+
 	res = atoi(argv[1]) * atoi(argv[2]);
 
-	if (argc != 3);
+	if (argc != 3)
 	{
 		printf("Error\n");
-		
+
 		return (1);
 	}
 	printf("%d\n", res);
