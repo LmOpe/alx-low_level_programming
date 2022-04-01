adca6ce85479c02c802498406fa0ea9854366328
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-01 08:52:54 -0700

    Update 4-add.c

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,34 +1,57 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "main.h"
+#include <stdio.h> /* printf */
+#include <stdlib.h> /* atoi */
+#include <stdbool.h> /* bool data type */
 
 /**
- * main - program that adds positive numbers
+ * is_num - iterate through each argv to test if it's a number
+ * @argvv: a argv
+ * Return: true only if entire string is a number, false if not
+ */
+
+bool is_num(char *argvv)
+{
+	int j = 0;
+
+	for (j = 0; argvv[j]; j++)
+	{
+		if (!(argvv[j] >= '0' && argvv[j] <= '9'))
+			return (0);
+	}
+	return (1);
+}
+
+/**
+ * main - print sum if all arguments given are numbers
  * @argc: argument counter
- * @argv: pointer to array of arguments
- * Return: 0 on success, 1 if there is a non digit arguments
+ * @argv: arguments
+ * Return: 0 on success, 1 if an argument wasn't a number
  */
 
 int main(int argc, char *argv[])
 {
-	int counter;
+	int i = 1;
+	int sum = 0;
 
+	/* validate input */
 	if (argc == 1)
 	{
 		printf("0\n");
+		return (0);
 	}
-	for (counter = 1; counter <= argc; counter++)
+
+	/* check all arguments to add numbers */
+	while (i < argc)
 	{
-		if (typeof(argv[counter]) != "int")
+		if (is_num(argv[i]))
+			sum += atoi(argv[i]);
+		else
 		{
 			printf("Error\n");
 			return (1);
 		}
-
-		atoi(argv[counter]) += atoi(argv[counter]);
+		i++;
 	}
-	pintf("%d\n", atoi(argv[counter]));
+	printf("%d\n", sum);
 
 	return (0);
 }
-
