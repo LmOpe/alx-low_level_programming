1543e404917efe9f0bb2ba825689881427a5a255
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-01 07:22:55 -0700

    Update 1-args.c

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
-#include "main.h"
+#include "holberton.h"
 
 /**
- * main - a program that prints the number of arguments passed into it.
+ * main - print number of arguments passed to program
  * @argc: argument counter
  * @argv: pointer to array of arguments
- * Return: Always 0(success)
+ * Return: 0 on success
  */
 
-int main (int argc, char *argv[] __attribute__((unused)))
+int main(int argc, char *argv[] __attribute__((unused)))
 {
-  if (argc > 0)
-  {
-    printf("%d\n", argc - 1);
-  }
-  
-  return (0);
+	if (argc > 0)
+		printf("%d\n", argc - 1);
+
+	return (0);
 }
