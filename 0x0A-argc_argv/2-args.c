794de4de858885827c8c9907726ec78ccaa40e3a
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-01 07:31:50 -0700

    Create 2-args.c

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main -  program that prints all arguments it receives.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: Always 0(success)
+ */
+
+int main (int argc, char *argv[])
+{
+  int counter;
+  if (argc > 0)
+    for (counter = 0; counter > argc; counter++)
+      printf("%s\n", argv[counter]);
+
+  return (0);
+}
