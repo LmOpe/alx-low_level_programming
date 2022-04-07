1d3c8e1ca1d97e701a55c7f3cc0591eba7e3a245
LmOpe <lawalmuhammedope@gmail.com>
2022-04-07 04:36:40 -0700

    Malloc

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -7,12 +7,13 @@
  * @argc: argument counter
  * @argv: pointer to array of arguments
  * exit: 98
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-  int num1;
-  int num2;
+	int num1;
+	int num2;
 
 	if (argc != 3)
 	{
@@ -20,13 +21,14 @@ int main(int argc, char *argv[])
 
 		exit(98);
 	}
-  num1 = atoi(argv[1]);
-  num2 = atoi(argv[2]);
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
 
-  if(num1 >= '0' && num1 <= '9' &&  num2 >= '0' && num2 <= '9')
-  {
-	printf("%d\n", num1 * num2);
-  }
-  printf("Error\n");
+	if (num1 >= '0' && num1 <= '9' &&  num2 >= '0' && num2 <= '9')
+	{
+		printf("%d\n", num1 * num2);
+		return (0);
+	}
+	printf("Error\n");
 	exit(98);
 }
