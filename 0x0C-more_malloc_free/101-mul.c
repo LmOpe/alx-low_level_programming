c895be07367cb6c426dc178e08e34861156ead79
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-07 04:27:51 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,11 +1,32 @@
 #include <stdio.h>
+#include "main.h"
+#include <stdlib.h>
 
 /**
- * main - program that multiplies two numbers
+ * main - program that multiplies two numbers.
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
  * exit: 98
  */
 
-int main(void)
+int main(int argc, char *argv[])
 {
+  int num1;
+  int num2;
 
+	if (argc != 3)
+	{
+		printf("Error\n");
+
+		exit(98);
+	}
+  num1 = atoi(argv[1]);
+  num2 = atoi(argv[2]);
+  
+  if(num1 >= '0' && num1 <= '9' &&  num2 >= '0' && num2 <= '9')
+  {
+	printf("%d\n", num1 * num2);
+  }
+  printf("Error\n");
+	exit(98);
 }
