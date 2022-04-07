164692f83b3e2022bddcd6aaf4a05e918ce19350
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-07 04:47:40 -0700

    Update 101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -24,7 +24,7 @@ int main(int argc, char *argv[])
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[2]);
 
-	if (num1 >= '0' && num1 <= '9' &&  num2 >= '0' && num2 <= '9')
+	if (isdigit(num1) && isdigit(num2))
 	{
 		printf("%d\n", num1 * num2);
 		return (0);
