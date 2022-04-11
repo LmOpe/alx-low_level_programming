84bc411a928a660d32d0b94bd6634f8eb905425c
LmOpe <lawalmuhammedope@gmail.com>
2022-04-11 02:05:53 -0700

    Preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints the name of the file it was compiled from
+ * @argc: argument counter
+ * @argv: pointer to argument
+ * Return: Always 0(success)
+ */
+
+int main (int argc, char * argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
