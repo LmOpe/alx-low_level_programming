2a645f7a4123b2fa8984dc5d8a092ddd5d3701ae
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-11 04:34:16 -0700

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 
 /**
- * main - program that prints the name of the file it was compiled from
+ * main - print name of program
  * @argc: argument counter
- * @argv: pointer to argument
- * Return: Always 0(success)
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-	printf("%s\n", argv[0]);
+	if (argc > 0)
+		printf("%s\n", argv[0]);
+
 	return (0);
 }
