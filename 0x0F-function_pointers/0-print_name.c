9d268a57bdb6264e0d2e4adb5d55f2b3dd00b99d
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-13 06:21:39 -0700

    Create 0-print_name.c

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * print_name - function that prints name
+ * @name: name of the person
+ * @f: pointer to the function
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+ int i = 0;
+  
+  while (name[i] != '\0')
+  {
+    _putchar(name[i]);
+    i++;
+  }
+}
