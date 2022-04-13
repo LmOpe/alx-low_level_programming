f4976ce717116e1d4e8d654b307020fbc4240bd2
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-13 06:38:54 -0700

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -2,12 +2,13 @@
 #include "function_pointers.h"
 
 /**
- * print_name - function that prints name
+ * print_name - function that calls a function that prints name
  * @name: name of the person
  * @f: pointer to the function
  * Return: nothing
  */
 void print_name(char *name, void (*f)(char *))
 {
-	printf("%s", name);
+	if (name != NULL && f != NULL)
+		f(name);
 }
