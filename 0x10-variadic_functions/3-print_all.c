2b7e52f8a0218e03385572d66b2768ee93f1c701
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-15 16:01:32 -0700

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,6 +1,53 @@
 #include "variadic_functions.h"
 
 /**
+ * print_all - Function that prints its parameter
+ * @format: first arg
+ * Return: void
+ */
+
+void print_all(const char * const format, ...)
+{
+	int n, m, p = 0;
+	char *s = "cifs";
+	va_list ap;
+
+	void (*funcs[])(va_list *) = {print_char, print_int, print_float, print_str};
+
+	va_start(ap, format);
+	n = 0;
+
+	while (*(format + n) != '\0' && format != NULL)
+	{
+		m = 0;
+		while (*(s + m) != '\0')
+		{
+			if (*(format + n) == *(s + m))
+			{
+				switch (p)
+				{
+					case 1:
+						printf(", ");
+						break;
+				}
+
+				funcs[m](&ap);
+				p = 1;
+				break;
+			}
+
+			m++;
+		}
+
+		n++;
+	}
+
+	va_end(ap);
+	printf("\n");
+
+}
+
+/**
  * print_char - prints character
  * @ap: character to be printed
  * Return: void
@@ -49,50 +96,4 @@ void print_str(va_list *ap)
 		return;
 	}
 	printf("%s", cs);
-}
-
-/**
- * print_all - Function that prints its parameter
- * @format: first arg
- * Return: void
- */
-
-void print_all(const char * const format, ...)
-{
-	int n, m, p = 0;
-	char *s = "cifs";
-	va_list ap;
-
-	void (*funcs[])(va_list *) = {print_char, print_int, print_float, print_str};
-
-	va_start(ap, format);
-	n = 0;
-
-	while (*(format + n) != '\0' && format != NULL)
-	{
-		m = 0;
-		while (*(s + m) != '\0')
-		{
-			if (*(format + n) == *(s + m))
-			{
-				switch (p)
-				{
-					case 1:
-						printf(", ");
-						break;
-				}
-
-				funcs[m](&ap);
-				p = 1;
-				break;
-			}
-
-			m++;
-		}
-
-		n++;
-	}
-
-	va_end(ap);
-	printf("\n");
 }
