2e17bead87fc4cb210f48ad1fdbc6a3b06535d18
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-24 07:31:16 -0700

    Update 100-first.c

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,13 +1,13 @@
 #include "lists.h"
 
-void startupFunc (void) __attribute__ ((constructor));
+void startupFunc(void) __attribute__ ((constructor));
 
 /**
  * startupFunc - function that get invoked before main function
  * Return: void
  */
 
-void startupFunc (void) 
+void startupFunc(void)
 {
-  printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 }
