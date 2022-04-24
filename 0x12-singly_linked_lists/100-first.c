300355188b5104eb92b7cd429672a1b1f44fbeab
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-24 07:33:40 -0700

    lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -9,5 +9,6 @@ void startupFunc(void) __attribute__ ((constructor));
 
 void startupFunc(void)
 {
-printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
+printf("You're beat! and yet, you must allow,")
+printf("I bore my house upon my back!\n");
 }
