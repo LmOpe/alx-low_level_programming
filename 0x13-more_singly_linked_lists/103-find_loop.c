074e8206d25819f213d4ce510a17ec020074c1c9
Muhammed <90075667+LmOpe@users.noreply.github.com>
2022-04-26 07:24:18 -0700

    Update 103-find_loop.c

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -1,39 +1,29 @@
 #include "lists.h"
 
 /**
- * find_listint_loop - find the loop in a linked list
- * @head: head of linked list
- * Description: Not allowed to use malloc, free or arrays.
- * Can only declare a max of 2 variables.
- * Return: Address of node where loop starts, or NULL if no loop found.
+ * reverse_listint - reverse linked list
+ * @head: pointer to head pointer of linked list
+ * Return: pointer to first node of reversed list
  */
- 
-listint_t *find_listint_loop(listint_t *head)
-{
-	listint_t *currents, *currentf;
 
-	if (head == NULL)
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *prev, *next;
+
+	/* account for no ptr and empty list */
+	if (head == NULL || *head == NULL)
 		return (NULL);
 
-	currents = currentf = head;
-	do {
-		if (currents->next)
-			currents = currents->next;
-		else
-			return (NULL);
-
-		if (currentf->next->next)
-			currentf = currentf->next->next;
-		else
-			return (NULL);
-	} while (currentf != currents);
-
-	currents = head;
-	while (currentf != currents)
+	/* iterate thorugh list to reverse linking */
+	prev = NULL;
+	while (*head != NULL)
 	{
-		currentf = currentf->next;
-		currents = currents->next;
+		next = (*head)->next; /* keep track of next node to move to */
+		(*head)->next = prev; /* link current node's ptr to prev node */
+		prev = *head; /* update previous node to be current node */
+		*head = next; /* move current node to next node */
 	}
+	(*head) = prev; /* unlink last element from null and point backwards */
 
-	return (currents);
+	return (*head);
 }
